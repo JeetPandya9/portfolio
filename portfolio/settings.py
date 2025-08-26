ba78dc6991b253240fc94285c8ea4a6e7c4ab2f4
JeetPandya9 <pandyadharmendra38@gmail.com>
2025-08-26 14:49:51 +0530

    debug=false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1rcms$-4txcwu817_uyv4@f*u=$!c1iwf2ayiwzox8lqvva((_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "portfolio2-jeet-pandya.onrender.com",
